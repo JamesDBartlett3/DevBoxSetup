8af841e5167fe1df5aad7cf9d83fbaa82b2c2260
James Bartlett <jbartlett@dmu.edu>
2023-02-15 11:17:57 -0600

    refactored admin re-launch, added comments

--- Registry/LongPathsEnabled_Registry_Tweak.ps1
+++ Registry/LongPathsEnabled_Registry_Tweak.ps1
@@ -1,5 +1,8 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-	[Security.Principal.WindowsBuiltInRole] "Administrator")) { `
-	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" `
-	-Verb RunAs; exit }
+# Get current PowerShell session process name
+[string]$ps = (get-process -id $pid).ProcessName
+
+# Check if script is running as administrator. If not, restart as administrator.
+if(!!${env:=::}){Start-Process $ps "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit}
+
+# Enable long paths
 New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD -Force
--- Registry/MouseWheelInversion.ps1
+++ Registry/MouseWheelInversion.ps1
@@ -6,10 +6,12 @@
 # Reverse mouse wheel scroll: 1 
 # Normal mouse wheel scroll: 0 
 
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-	[Security.Principal.WindowsBuiltInRole] "Administrator")) { `
-	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" `
-	-Verb RunAs; exit }
+# Get current PowerShell session process name
+[string]$ps = (get-process -id $pid).ProcessName
 
+# Check if script is running as administrator. If not, restart as administrator.
+if(!!${env:=::}){Start-Process $ps "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit}
+
+# Set registry settings
 Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopWheel -EA 0 | ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopWheel 1 }
 Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopHScroll  -EA 0 | ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopHScroll 1 }
