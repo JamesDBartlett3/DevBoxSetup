b9233efa22182c1f0d625bc602be2c8db33dae79
James Bartlett <jbartlett@dmu.edu>
2023-06-26 15:10:10 -0500

    changed autocrlf to true

--- PowerShell/Setup-GitConfig.ps1
+++ PowerShell/Setup-GitConfig.ps1
@@ -1,4 +1,4 @@
 git config --system core.longpaths true
-git config --system core.autocrlf false
+git config --system core.autocrlf true
 git config --global core.compression 0
 git config --global fetch.prune true
