d96ee9a92dd3c7f60ec0968379d8be09e26de53b
James Bartlett <jbartlett@dmu.edu>
2023-06-26 15:30:37 -0500

    Changes: - Added 'System' and 'Global' headings - Moved 'core.autocrlf' from System to Global - Changed 'core.autocrlf' value from 'true' to 'input' - Added 'core.editor "code --wait"' to Global

--- PowerShell/Setup-GitConfig.ps1
+++ PowerShell/Setup-GitConfig.ps1
@@ -1,4 +1,8 @@
+# System (Requires Admin)
 git config --system core.longpaths true
-git config --system core.autocrlf true
+
+# Global
+git config --global core.autocrlf input
 git config --global core.compression 0
+git config --global core.editor "code --wait"
 git config --global fetch.prune true
