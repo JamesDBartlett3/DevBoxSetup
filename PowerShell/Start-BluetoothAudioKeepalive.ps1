97439dfa8e1d2391640a4d1429f307e04fcb18cc
James Bartlett <jbartlett@dmu.edu>
2023-07-13 13:30:19 -0500

    updated comment block

--- PowerShell/Start-BluetoothAudioKeepalive.ps1
+++ PowerShell/Start-BluetoothAudioKeepalive.ps1
@@ -1,15 +1,14 @@
-<################################################################/
+<##########################################################################/
 
 Author: @JamesDBartlett3@techhub.social
 
-Plays a tone with x frequency (in Hz) for y miliseconds every z seconds
-to prevent Bluetooth audio devices from auto-disconnecting due to inactivity.
+Infinite loop that plays a tone with x frequency (in Hz) for y miliseconds 
+to prevent audio devices from sleeping due to inactivity.
 
-For best results, choose a tone frequency above normal human hearing range (20,000+),
-a length of about 1 second (1000 ms), and a delay just a bit shorter than your 
-Bluetooth device's timeout length.
+For best results, choose a tone frequency above normal human hearing range 
+(20,000+ Hz) and a length of several hours.
 
-/################################################################>
+/##########################################################################>
 
 [CmdletBinding()]
 Param (
