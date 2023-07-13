4faff1dec30ebd4dc1e42365d0f2fc36bdd87abd
James Bartlett <jbartlett@dmu.edu>
2023-07-13 13:37:04 -0500

    set autocrlf to true for Windows

--- PowerShell/Setup-GitConfig.ps1
+++ PowerShell/Setup-GitConfig.ps1
@@ -2,7 +2,7 @@
 git config --system core.longpaths true
 
 # Global
-git config --global core.autocrlf input
+git config --global core.autocrlf true
 git config --global core.compression 0
 git config --global core.editor "code --wait"
 git config --global fetch.prune true
