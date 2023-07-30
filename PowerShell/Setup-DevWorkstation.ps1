6ea568ffc986cd79e81203b6318ec856e389f628
James D. Bartlett III <james.bartlett@gmail.com>
2023-07-30 18:54:57 -0500

    replated double-spaces with tabs

--- PowerShell/Setup-DevWorkstation.ps1
+++ PowerShell/Setup-DevWorkstation.ps1
@@ -24,14 +24,14 @@ Based on this gist: https://gist.github.com/Codebytes/29bf18015f6e93fca9421df73c
 #Install WinGet
 $hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
 if (!$hasPackageManager -or [version]$hasPackageManager.Version -lt [version]"1.10.0.0") {
-  "Installing winget Dependencies"
-  Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
-  $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
-  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-  $releases = Invoke-RestMethod -uri $releases_url
-  $latestRelease = $releases.assets | Where-Object { $_.browser_download_url.EndsWith('msixbundle') } | Select-Object -First 1
-  "Installing winget from $($latestRelease.browser_download_url)"
-  Add-AppxPackage -Path $latestRelease.browser_download_url
+	"Installing winget Dependencies"
+	Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+	$releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+	$releases = Invoke-RestMethod -uri $releases_url
+	$latestRelease = $releases.assets | Where-Object { $_.browser_download_url.EndsWith('msixbundle') } | Select-Object -First 1
+	"Installing winget from $($latestRelease.browser_download_url)"
+	Add-AppxPackage -Path $latestRelease.browser_download_url
 }
 
 #Configure WinGet
@@ -41,76 +41,76 @@ Write-Output "Configuring winget"
 $settingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
 $settingsJson = 
 @"
-  {
-    // For documentation on these settings, see: https://aka.ms/winget-settings
-    "experimentalFeatures": {
-      "experimentalMSStore": true,
-    }
-  }
+	{
+		// For documentation on these settings, see: https://aka.ms/winget-settings
+		"experimentalFeatures": {
+			"experimentalMSStore": true,
+		}
+	}
 "@
 $settingsJson | Out-File $settingsPath -Encoding utf8
 
 #Install New apps
 Write-Output "Installing Apps"
 $apps = @(
-  @{name = "Chocolatey.ChocolateyGUI" },
-  @{name = "Microsoft.Edge" },
-  @{name = "Microsoft.AzureCLI" }, 
-  @{name = "Microsoft.PowerShell" }, 
-  @{name = "Microsoft.VisualStudioCode" }, 
-  @{name = "Microsoft.WindowsTerminal" }, 
-  @{name = "Microsoft.AzureStorageExplorer" }, 
-  @{name = "Microsoft.PowerToys" }, 
-  @{name = "Microsoft.PowerBI" },
-  @{name = "Git.Git" },
-  @{name = "Microsoft.DotNet.Runtime.7" },
-  @{name = "Microsoft.DotNet.DesktopRuntime.7" },
-  # @{name = "Microsoft.DotNet.Runtime.6" },
-  # @{name = "Microsoft.DotNet.DesktopRuntime.6" },
-  @{name = "Microsoft.DotNet.SDK.7" },
-  # @{name = "Microsoft.DotNet.SDK.6" },
-  @{name = "GitHub.cli" },
-  @{name = "DaxStudio.DaxStudio" },
-  # @{name = "Tabular Editor"}, # No package currently available
-  # @{name = "GitHub.GitHubDesktop" },
-  @{name = "Microsoft.Git" },
-  @{name = "Microsoft.GitCredentialManagerCore" },
-  @{name = "GitHub.GitLFS" },
-  @{name = "JanDeDobbeleer.OhMyPosh" },
-  @{name = "ExplorerPatcher" }, # Patch Windows 11 Explorer to make Start Menu and Taskbar look like Windows 10
-  # @{name = "winpython.winpython" },
-  # @{name = "Canonical.Ubuntu" },
-  # @{name = "beekeeper-studio.beekeeper-studio" },
-  @{name = "gerardog.gsudo" },
-  # @{name = "" },
-  @{name = "Microsoft.AzureDataStudio" },
-  @{name = "Microsoft.AzureDataCLI" },
-  @{name = "Microsoft.AzureFunctionsCoreTools" },
-  @{name = "Microsoft.AzureStorageEmulator" },
-  @{name = "7zip.7zip" },
-  # @{name = "voidtools.Everything" },
-  @{name = "Sysinternals Suite"; source = "msstore"}, 
-  @{name = "Microsoft PowerToys"; source = "msstore"}, # PowerToys
-  @{name = "Microsoft.Sysinternals" }
-  @{name = "WinFsp.WinFsp" },
-  @{name = "SSHFS-Win.SSHFS-Win" },
-  @{name = "Protecc"; source = "msstore"}
+	@{name = "Chocolatey.ChocolateyGUI" },
+	@{name = "Microsoft.Edge" },
+	@{name = "Microsoft.AzureCLI" }, 
+	@{name = "Microsoft.PowerShell" }, 
+	@{name = "Microsoft.VisualStudioCode" }, 
+	@{name = "Microsoft.WindowsTerminal" }, 
+	@{name = "Microsoft.AzureStorageExplorer" }, 
+	@{name = "Microsoft.PowerToys" }, 
+	@{name = "Microsoft.PowerBI" },
+	@{name = "Git.Git" },
+	@{name = "Microsoft.DotNet.Runtime.7" },
+	@{name = "Microsoft.DotNet.DesktopRuntime.7" },
+	# @{name = "Microsoft.DotNet.Runtime.6" },
+	# @{name = "Microsoft.DotNet.DesktopRuntime.6" },
+	@{name = "Microsoft.DotNet.SDK.7" },
+	# @{name = "Microsoft.DotNet.SDK.6" },
+	@{name = "GitHub.cli" },
+	@{name = "DaxStudio.DaxStudio" },
+	# @{name = "Tabular Editor"}, # No package currently available
+	# @{name = "GitHub.GitHubDesktop" },
+	@{name = "Microsoft.Git" },
+	@{name = "Microsoft.GitCredentialManagerCore" },
+	@{name = "GitHub.GitLFS" },
+	@{name = "JanDeDobbeleer.OhMyPosh" },
+	@{name = "ExplorerPatcher" }, # Patch Windows 11 Explorer to make Start Menu and Taskbar look like Windows 10
+	# @{name = "winpython.winpython" },
+	# @{name = "Canonical.Ubuntu" },
+	# @{name = "beekeeper-studio.beekeeper-studio" },
+	@{name = "gerardog.gsudo" },
+	# @{name = "" },
+	@{name = "Microsoft.AzureDataStudio" },
+	@{name = "Microsoft.AzureDataCLI" },
+	@{name = "Microsoft.AzureFunctionsCoreTools" },
+	@{name = "Microsoft.AzureStorageEmulator" },
+	@{name = "7zip.7zip" },
+	# @{name = "voidtools.Everything" },
+	@{name = "Sysinternals Suite"; source = "msstore"}, 
+	@{name = "Microsoft PowerToys"; source = "msstore"}, # PowerToys
+	@{name = "Microsoft.Sysinternals" }
+	@{name = "WinFsp.WinFsp" },
+	@{name = "SSHFS-Win.SSHFS-Win" },
+	@{name = "Protecc"; source = "msstore"}
 )
 
 winget list --accept-source-agreements | Out-Null;
 Foreach ($app in $apps) {
-  $listApp = winget list --exact -q $app.name
-  if (![String]::Join("", $listApp).Contains($app.name)) {
-    Write-Host "Installing:" $app.name
-    if ($null -ne $app.source) {
-      winget install --exact --silent $app.name --source $app.source
-    }
-    else {
-      winget install --exact --silent $app.name 
-    }
-  } else {
-    Write-Host "Skipping Install of " $app.name
-  }
+	$listApp = winget list --exact -q $app.name
+	if (![String]::Join("", $listApp).Contains($app.name)) {
+		Write-Host "Installing:" $app.name
+		if ($null -ne $app.source) {
+			winget install --exact --silent $app.name --source $app.source
+		}
+		else {
+			winget install --exact --silent $app.name 
+		}
+	} else {
+		Write-Host "Skipping Install of " $app.name
+	}
 }
 
 #Remove Apps
@@ -119,8 +119,8 @@ Write-Output "Removing Bloatware Apps..."
 $bloatware = "*3DPrint*", "Microsoft.MixedReality.Portal", "*Xbox*", "Microsoft.Getstarted*"
 Foreach ($app in $bloatware)
 {
-  Write-Host "Uninstalling:" $app
-  Get-AppxPackage -allusers $app | Remove-AppxPackage
+	Write-Host "Uninstalling:" $app
+	Get-AppxPackage -allusers $app | Remove-AppxPackage
 }
 
 # Install Chocolatey
