9c498a5962f67ce0a4720078599fd93f92eaf089
James D. Bartlett III <james.bartlett@gmail.com>
2023-12-24 10:45:40 -0600

    converted tabs to spaces

--- PowerShell/Install-PSModules.ps1
+++ PowerShell/Install-PSModules.ps1
@@ -15,102 +15,102 @@ Author: @JamesDBartlett3 (https://techhub.social/@JamesDBartlett3)
 [System.Net.ServicePointManager]::DnsRefreshTimeout = 0
 
 if ($PSVersionTable.PSVersion.Major -lt 7) {
-	Write-Output "Please run this script with PowerShell Core version 7.0 or later."
-	Write-Output "Press any key to exit..."
-	$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-	exit
+  Write-Output "Please run this script with PowerShell Core version 7.0 or later."
+  Write-Output "Press any key to exit..."
+  $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+  exit
 }
 else {
-	Function New-Separator {
-		[CmdletBinding()]
-		Param(
-			[Parameter(Mandatory = $false)]
-			[int]$Length = $Host.UI.RawUI.WindowSize.Width
-		)
-		Write-Host ('-' * $Length)
-	}
+  Function New-Separator {
+    [CmdletBinding()]
+    Param(
+      [Parameter(Mandatory = $false)]
+      [int]$Length = $Host.UI.RawUI.WindowSize.Width
+    )
+    Write-Host ('-' * $Length)
+  }
 
-	# $isAdmin = (
-	# 	[Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
-	# 	).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+  # $isAdmin = (
+  # 	[Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
+  # 	).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 
-	# if (!$isAdmin){
-	# 	$a = "-NoProfile -NonInteractive -ExecutionPolicy Bypass -File `"$PSCommandPath`""
-	# 	Start-Process pwsh.exe $a -Verb RunAs; exit}
+  # if (!$isAdmin){
+  # 	$a = "-NoProfile -NonInteractive -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+  # 	Start-Process pwsh.exe $a -Verb RunAs; exit}
 
-	# Declare list of PowerShell modules to install
-	[array]$modules = @(
-		"Az.Accounts"
-		, "Az.AnalysisServices"
-		, "Az.ApiManagement"
-		, "Az.AppConfiguration"
-		, "Az.DataFactory"
-		, "Az.DataLakeAnalytics"
-		, "Az.DataLakeStore"
-		, "Az.Functions"
-		, "Az.LogAnalytics"
-		, "Az.PowerBIEmbedded"
-		, "Az.Resources"
-		, "Az.Sql"
-		, "Az.Storage"
-		, "Az.Synapse"
-		, "AzureAD"
-		# , "AzureADPreview"
-		, "DataGateway"
-		, "DataGateway.Profile"
-		# , "DataMashup" # requires -AllowPrerelease
-		, "dbatools"
-		, "dbops"
-		, "DynamicTitle"
-		, "ExchangePowerShell"
-		, "F7History"
-		, "Fabtools" # https://github.com/Jojobit/Fabtools
-		, "ImportExcel"
-		, "InvokeBuild"
-		, "Metadata"
-		, "MicrosoftPowerBIMgmt"
-		, "MicrosoftTeams"
-		, "Microsoft.Graph"
-		, "Microsoft.Online.SharePoint.PowerShell"
-		, "Microsoft.PowerShell.ConsoleGuiTools"
-		, "ModuleBuilder"
-		, "MSAL.PS" # https://github.com/AzureAD/MSAL.PS
-		, "MSOnline"
-		, "OnPremisesDataGatewayHAMgmt"
-		, "PnP.PowerShell" # https://github.com/pnp/powershell
-		, "posh-git"
-		, "PowerHTML"
-		, "PowerShell-Beautifier"
-		, "PowerShellForGitHub"
-		, "PowerShellGet"
-		, "PowerShellNotebook"
-		, "PowerShellProTools"
-		, "ps2exe"
-		, "psedit" # https://github.com/ironmansoftware/psedit
-		, "PSFramework"
-		, "PSKoans"
-		, "PSReadLine"
-		, "PSRequiredModules"
-		, "PSScriptAnalyzer"
-		, "PSScriptTools"
-		, "ReportingServicesTools"
-		, "SqlServer"
-		, "ugit" # https://github.com/StartAutomating/ugit
-	)
-	
-	# Set InstallationPolicy for PSGallery repository to Trusted
-	Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+  # Declare list of PowerShell modules to install
+  [array]$modules = @(
+    "Az.Accounts"
+    , "Az.AnalysisServices"
+    , "Az.ApiManagement"
+    , "Az.AppConfiguration"
+    , "Az.DataFactory"
+    , "Az.DataLakeAnalytics"
+    , "Az.DataLakeStore"
+    , "Az.Functions"
+    , "Az.LogAnalytics"
+    , "Az.PowerBIEmbedded"
+    , "Az.Resources"
+    , "Az.Sql"
+    , "Az.Storage"
+    , "Az.Synapse"
+    , "AzureAD"
+    # , "AzureADPreview"
+    , "DataGateway"
+    , "DataGateway.Profile"
+    # , "DataMashup" # requires -AllowPrerelease
+    , "dbatools"
+    , "dbops"
+    , "DynamicTitle"
+    , "ExchangePowerShell"
+    , "F7History"
+    , "Fabtools" # https://github.com/Jojobit/Fabtools
+    , "ImportExcel"
+    , "InvokeBuild"
+    , "Metadata"
+    , "MicrosoftPowerBIMgmt"
+    , "MicrosoftTeams"
+    , "Microsoft.Graph"
+    , "Microsoft.Online.SharePoint.PowerShell"
+    , "Microsoft.PowerShell.ConsoleGuiTools"
+    , "ModuleBuilder"
+    , "MSAL.PS" # https://github.com/AzureAD/MSAL.PS
+    , "MSOnline"
+    , "OnPremisesDataGatewayHAMgmt"
+    , "PnP.PowerShell" # https://github.com/pnp/powershell
+    , "posh-git"
+    , "PowerHTML"
+    , "PowerShell-Beautifier"
+    , "PowerShellForGitHub"
+    , "PowerShellGet"
+    , "PowerShellNotebook"
+    , "PowerShellProTools"
+    , "ps2exe"
+    , "psedit" # https://github.com/ironmansoftware/psedit
+    , "PSFramework"
+    , "PSKoans"
+    , "PSReadLine"
+    , "PSRequiredModules"
+    , "PSScriptAnalyzer"
+    , "PSScriptTools"
+    , "ReportingServicesTools"
+    , "SqlServer"
+    , "ugit" # https://github.com/StartAutomating/ugit
+  )
+  
+  # Set InstallationPolicy for PSGallery repository to Trusted
+  Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 
-	# Loop through $modules object and install each module
-	foreach ($module in $modules) {
-		New-Separator
-		Write-Output "Installing module: '$module'..."
-		Install-Module -Name $module -Scope CurrentUser -Repository PSGallery -AllowClobber -AcceptLicense
-	}
+  # Loop through $modules object and install each module
+  foreach ($module in $modules) {
+    New-Separator
+    Write-Output "Installing module: '$module'..."
+    Install-Module -Name $module -Scope CurrentUser -Repository PSGallery -AllowClobber -AcceptLicense
+  }
 
-	New-Separator
+  New-Separator
 
-	# Update local help cache
-	Update-Help -ErrorAction SilentlyContinue
+  # Update local help cache
+  Update-Help -ErrorAction SilentlyContinue
 
 }
