7fd2b47384bd6c046748b5e3940e365f646b9c83
James D. Bartlett III <james.bartlett@gmail.com>
2023-12-24 11:33:08 -0600

    formatting

--- PowerShell/Upgrade-PSModulesAndUninstallOldVersions.ps1
+++ PowerShell/Upgrade-PSModulesAndUninstallOldVersions.ps1
@@ -21,11 +21,11 @@ TODO: Refactor using this example: https://github.com/itpro-tips/PowerShell-Tool
 [System.Net.ServicePointManager]::DnsRefreshTimeout = 0
 
 Function Add-Separator {
-    Param(
-        [Parameter(Mandatory=$false)]
-        [int]$Width = $Host.UI.RawUI.WindowSize.Width
-    )
-    Write-Host ('-' * $Width)
+  Param(
+    [Parameter(Mandatory=$false)]
+    [int]$Width = $Host.UI.RawUI.WindowSize.Width
+  )
+  Write-Host ('-' * $Width)
 }
 
 Add-Separator
@@ -41,45 +41,45 @@ $mods = Get-InstalledModule | Compare-Module |
 
 # For each module
 foreach ($m in $mods) {
-
+	
   $name = $m.Name
-
+	
   # If the module has an update available, install it
   if ($m.UpdateNeeded) {
-    Write-Host "Updating '$name' module..." `
-      -ForegroundColor Yellow -BackgroundColor Black
-    Update-Module -Name $name -AcceptLicense
+  Write-Host "Updating '$name' module..." `
+    -ForegroundColor Yellow -BackgroundColor Black
+  Update-Module -Name $name -AcceptLicense
   }
-
+	
   # Get list of all currently installed versions of the module, sorted by Published Date
   $installedVersions = Get-InstalledModule -Name $name -AllVersions |
-    Sort-Object -Property PublishedDate
-
+  Sort-Object -Property PublishedDate
+	
   # If more than one version is installed...
   if ($installedVersions.Count -gt 1) { #TODO: This condition most likely never returns false, so it is probably unnecessary
-
-    # Get total number of versions installed and the version number of the latest version
-    $oldVersionsCount = $installedVersions.Count - 1
-    $oldVersions = $installedVersions | Select-Object -First $oldVersionsCount
-    $latestVersion = $installedVersions[-1].Version
-    Write-Host "Latest version of '$name' module (version $latestVersion) is now installed." `
-      -ForegroundColor Green -BackgroundColor Black
-
-    # Uninstall each old version
-    foreach ($v in $oldVersions) {
-
-      $name = $v.Name
-      $version = $v.Version
-      Write-Host "Uninstalling outdated version of '$name' module (version $version)..." `
-        -ForegroundColor Red -BackgroundColor Black
-      Uninstall-Module $v -Force
-
-    }
-
-    Add-Separator
-
+	
+  # Get total number of versions installed and the version number of the latest version
+  $oldVersionsCount = $installedVersions.Count - 1
+  $oldVersions = $installedVersions | Select-Object -First $oldVersionsCount
+  $latestVersion = $installedVersions[-1].Version
+  Write-Host "Latest version of '$name' module (version $latestVersion) is now installed." `
+    -ForegroundColor Green -BackgroundColor Black
+	
+  # Uninstall each old version
+  foreach ($v in $oldVersions) {
+	
+    $name = $v.Name
+    $version = $v.Version
+    Write-Host "Uninstalling outdated version of '$name' module (version $version)..." `
+    -ForegroundColor Red -BackgroundColor Black
+    Uninstall-Module $v -Force
+	
   }
-
+	
+  Add-Separator
+	
+  }
+	
 }
 
 Write-Host "Finished updating modules and uninstalling old versions." `
