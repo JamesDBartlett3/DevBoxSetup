5f782bdeb8f2af0d176b9aba1827a7a6542ac302
JamesDBartlett3 <37491308+jamesdbartlett3@users.noreply.github.com>
2025-01-15 16:39:01 -0600

    Implement Download-AppxPackage function to streamline Microsoft Store app installation

--- WindowsSandbox/WindowsSandbox_Install_MSStore_PBID_PBIRB.ps1
+++ WindowsSandbox/WindowsSandbox_Install_MSStore_PBID_PBIRB.ps1
@@ -1,5 +1,73 @@
 # TODO: Replace bonguides scripts with emdedded scripts that do the same thing
 # Reference: https://serverfault.com/questions/1018220/how-do-i-install-an-app-from-windows-store-using-powershell
+# Download-AppxPackage "https://www.microsoft.com/store/productId/9WZDNCRFJBMP" "C:\Support\Store"
+# Get-ChildItem C:\Support\Store\ | Add-AppxPackage
+
+function Download-AppxPackage {
+  [CmdletBinding()]
+  param (
+    [string]$Uri,
+    [string]$Path = "C:\Support\Store"
+  )
+
+    begin {
+      if (-Not (Test-Path "$Path")) {
+        Write-Host -ForegroundColor Green "Creating directory $Path"
+        New-Item -ItemType Directory -Force -Path "$Path"
+      }
+    }
+     
+    process {
+      Write-Output ""
+      $StopWatch = [system.diagnostics.stopwatch]::startnew()
+      $Path = (Resolve-Path $Path).Path
+      #Get Urls to download
+      Write-Host -ForegroundColor Yellow "Processing $Uri"
+      $WebResponse = Invoke-WebRequest -UseBasicParsing -Method 'POST' -Uri 'https://store.rg-adguard.net/api/GetFiles' -Body "type=url&url=$Uri&ring=Retail" -ContentType 'application/x-www-form-urlencoded'
+      $LinksMatch = ($WebResponse.Links | Where-Object {$_ -like '*.appx*'} | Where-Object {$_ -like '*_neutral_*' -or $_ -like "*_"+$env:PROCESSOR_ARCHITECTURE.Replace("AMD","X").Replace("IA","X")+"_*"} | Select-String -Pattern '(?<=a href=").+(?=" r)').matches.value
+      $Files = ($WebResponse.Links | Where-Object {$_ -like '*.appx*'} | Where-Object {$_ -like '*_neutral_*' -or $_ -like "*_"+$env:PROCESSOR_ARCHITECTURE.Replace("AMD","X").Replace("IA","X")+"_*"} | Where-Object {$_ } | Select-String -Pattern '(?<=noreferrer">).+(?=</a>)').matches.value
+      #Create array of links and filenames
+      $DownloadLinks = @()
+      for($i = 0;$i -lt $LinksMatch.Count; $i++){
+          $Array += ,@($LinksMatch[$i],$Files[$i])
+      }
+      #Sort by filename descending
+      $Array = $Array | sort-object @{Expression={$_[1]}; Descending=$True}
+      $LastFile = $null
+      for($i = 0;$i -lt $LinksMatch.Count; $i++){
+          $CurrentFile = $Array[$i][1]
+          $CurrentUrl = $Array[$i][0]
+          #Find first number index of current and last processed filename
+          if ($CurrentFile -match "(?<number>\d)"){}
+          $FileIndex = $CurrentFile.indexof($Matches.number)
+          if ($LastFile -match "(?<number>\d)"){}
+          $LastFileIndex = $LastFile.indexof($Matches.number)
+  
+          #If current filename product not equal to last filename product
+          if (($CurrentFile.SubString(0,$FileIndex-1)) -ne ($LastFile.SubString(0,$LastFileIndex-1))) {
+              #If file not already downloaded, add to the download queue
+              if (-Not (Test-Path "$Path\$CurrentFile")) {
+                  "Downloading $Path\$CurrentFile"
+                  $FilePath = "$Path\$CurrentFile"
+                  $FileRequest = Invoke-WebRequest -Uri $CurrentUrl -UseBasicParsing #-Method Head
+                  [System.IO.File]::WriteAllBytes($FilePath, $FileRequest.content)
+              }
+          #Delete file outdated and already exist
+          }elseif ((Test-Path "$Path\$CurrentFile")) {
+              Remove-Item "$Path\$CurrentFile"
+              "Removing $Path\$CurrentFile"
+          }
+          $LastFile = $CurrentFile
+      }
+      "Time to process: "+$StopWatch.ElapsedMilliseconds
+    }
+  }
+
+# Download the Microsoft Store
+Download-AppxPackage "https://www.microsoft.com/store/productId/9WZDNCRFJBMP"
+
+# Install the Microsoft Store
+Get-ChildItem "C:\Support\Store\" | Add-AppxPackage
 
 Start-Process powershell {-NoExit
 
@@ -8,7 +76,7 @@ Start-Process powershell {-NoExit
     Sort-Object -Property LastWriteTime -Descending)[0].FullName + '\Downloads' | Invoke-Item; 
 
   # Install the Microsoft Store
-  Invoke-RestMethod 'bonguides.com/wsb/msstore' | Invoke-Expression; 
+  # Invoke-RestMethod 'bonguides.com/wsb/msstore' | Invoke-Expression; 
 
   # Install Power BI Desktop and Power BI Report Builder
   if ((Read-Host 'Install Power BI Report Builder and Power BI Desktop now (Y/N)?').ToUpper() -eq 'Y') {
